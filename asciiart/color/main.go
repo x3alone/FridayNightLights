093f0ded436be88cafe57f040d1c23d357c74145
otmane chouari <oottmmaann3@gmail.com>
2024-05-18 19:03:24 +0100

    wow thank god

--- asciiart/color/main.go
+++ asciiart/color/main.go
@@ -1,20 +1,23 @@
 package main
+
 import (
 	"fmt"
 	"os"
 	"strings"
 )
+
 func read_file(file string) []string {
 	data, err := os.ReadFile(file)
 	if err != nil {
-		fmt.Print("Error : reading the file\n")
+		fmt.Print("Error: reading the file\n")
 		os.Exit(1)
 	}
 	split := strings.Split(string(data), "\n\n")
 	split[0] = strings.TrimPrefix(split[0], "\n")
 	return split
 }
-func array_2d(data []string) [][]string { 
+
+func array_2d(data []string) [][]string {
 	two_d_array := make([][]string, len(data))
 	for i, line := range data {
 		lines := strings.Split(line, "\n")
@@ -22,13 +25,43 @@ func array_2d(data []string) [][]string {
 	}
 	return two_d_array
 }
-func contains(input string, c rune) bool {
-	return strings.ContainsRune(input, c)
+
+func GetColor(color string) string {
+	switch strings.ToLower(color) {
+	case "red":
+		return "\033[31m"
+	case "green":
+		return "\033[32m"
+	case "yellow":
+		return "\033[33m"
+	case "blue":
+		return "\033[34m"
+	case "magenta":
+		return "\033[35m"
+	case "cyan":
+		return "\033[36m"
+	case "white":
+		return "\033[37m"
+	default:
+		return "\033[0m"
+	}
 }
-func print_shapes(shape [][]string, str string) {
-	color := os.Args[2][8:] 
-	input := os.Args[3]
+
+// func Matched(str, substr, colorCode string) string {
+// 	var result strings.Builder
+// 	for _, c := range str {
+// 		if strings.ContainsRune(substr, c) {
+// 			result.WriteString(fmt.Sprintf("%s%c%s", colorCode, c, "\033[0m"))
+// 		} else {
+// 			result.WriteRune(c)
+// 		}
+// 	}
+// 	return result.String()
+// }
+
+func print_shapes(shape [][]string, str string, color string, input string) {
 	i := 0
+	// tmp := ""
 	var to_print []string
 	var new []string
 	str = strings.ReplaceAll(str, "\\n", "\n")
@@ -37,15 +70,19 @@ func print_shapes(shape [][]string, str string) {
 	} else {
 		new = append(new, str)
 	}
-	colorCode := getColorCode(color)
+	colorCode := GetColor(color)
 
 	for _, line := range new {
 		if line != "" {
 			for i < 8 {
-				for _, c := range line {
-					if contains(input, c) {
-						// Print the character in the specified color
-						to_print = append(to_print, fmt.Sprintf("%s%s%s", colorCode, shape[int(c)-32][i], "\033[0m"))
+				for index1, c := range line {
+					if strings.Contains(line, input) {
+						index := strings.Index(line, input)
+						if index1 >= index && index1 <= index+len(input)-1 {
+							to_print = append(to_print, fmt.Sprintf("%s%s%s", colorCode, shape[int(c)-32][i], "\033[0m"))
+						} else {
+							to_print = append(to_print, shape[int(c)-32][i])
+						}
 					} else {
 						to_print = append(to_print, shape[int(c)-32][i])
 					}
@@ -61,17 +98,19 @@ func print_shapes(shape [][]string, str string) {
 		}
 	}
 }
+
 func check_input(str string) bool {
 	i := 0
 	for range str {
 		if (str[i] < 32 || str[i] > 126) && str[i] != '\n' {
-			fmt.Printf("\033[1m\033[31mError : Please provide a string with supported charachters!\n")
+			fmt.Printf("\033[1m\033[31mError: Please provide a string with supported characters!\n")
 			return true
 		}
 		i++
 	}
 	return false
 }
+
 func all_newline(str string) bool {
 	i := 0
 	for range str {
@@ -81,6 +120,7 @@ func all_newline(str string) bool {
 	}
 	return len(str) == i
 }
+
 func check_newline(str string) bool {
 	str = strings.ReplaceAll(str, "\\n", "\n")
 	if len(str) == 0 {
@@ -96,8 +136,19 @@ func check_newline(str string) bool {
 	}
 	return false
 }
+
+func checkflag(str string) string {
+	if strings.HasPrefix(str, "--color=") {
+		return str[8:]
+	}
+	if strings.HasPrefix(str, "--output=") {
+		return str[8:]
+	}
+	return ""
+}
+
 func main() {
-	if len(os.Args) > 2 && len(os.Args) < 4 {
+	if len(os.Args) >= 2 && len(os.Args) <= 4 {
 		if check_input(os.Args[1]) {
 			return
 		}
@@ -106,8 +157,12 @@ func main() {
 		}
 		data := read_file("standard.txt")
 		shape := array_2d(data)
-		print_shapes(shape, os.Args[1])
+		// color := os.Args[1][8:]
+		color := checkflag(os.Args[1])
+		input := os.Args[2]
+		print_shapes(shape, os.Args[3], color, input)
 	} else {
 		fmt.Printf("\033[1m\033[033mError: enter the correct arguments!\n")
 	}
 }
+
